7091c3398b1b8dc9c195552aec84de2c26e3981b
unknown <gw-danmace@hotmail.com>
2018-03-17 22:06:38 -0400

    some notes added

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+
+
+// clicking might need to be disabled on a timer
+// will need to bring in characters selected on landing page
+// sprites must appear, and perhaps move ie cycle through gifs or move picture location
+// can add different "arena" options if time
+// there should be a button to start the battle
+// will need to create global variables for hp
+// stats will need to be used to generate likelihood of attacking first, etc.
+// global variable for score must be created, and cleared out after [3?] matches
+// [3?] wins should result in an animation playing, and the spawning of buttons to either fight again or return to first page
+
+// create for loops
+// while hp self vs. opponent [value],
+Math.floor((Math.random() * 10) + 1);
+// if # = x-y, [action], etc.
+// link each action to relevant gif, or movement of image to new location
+// trigger sound effects, etc.
