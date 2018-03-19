f9299090af122707fd9ba329f7d9a85b56b31276
unknown <gw-danmace@hotmail.com>
2018-03-19 13:29:06 -0400

    more templates added, arena.html created

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,27 @@
+// template below //
 
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "OreoOreo29!",
+  database: "cat_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
 
 
 
